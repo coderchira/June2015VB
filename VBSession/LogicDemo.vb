b582c559c414794ddb1e3eaa6656eb40c870a77e
coderchira <ruchira.aggarwal@gmail.com>
2015-06-23 15:58:17 +0530

    logic demo

---
+++ VBSession/LogicDemo.vb
@@ -0,0 +1,21 @@
+﻿Module LogicDemo
+    'Logic
+    '   Functions
+    '       Return a value
+    '   Subroutines
+    '       Do not return a value
+
+    'Lifecycle of the logic block
+    '   Declare
+    '       use keyword function 
+    '       Name
+    '       return type
+    '   Implement / Body
+    '       use data here / form logic
+    '   Call
+    '       from other block
+
+    'Declare a function
+
+
+End Module
